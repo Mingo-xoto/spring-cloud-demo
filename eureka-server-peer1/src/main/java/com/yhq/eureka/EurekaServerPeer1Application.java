26a16aed4396a1ff4875ecb5327ac96f01c8f95d
杨华奇 <541871045@qq.com>
2017-06-29 17:33:43 +0800

    eureka集群相互注册

--- eureka-server-peer1/src/main/java/com/yhq/eureka/EurekaServerPeer1Application.java
+++ eureka-server-peer1/src/main/java/com/yhq/eureka/EurekaServerPeer1Application.java
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerPeer1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServerPeer1Application.class, args);
 	}
 }
