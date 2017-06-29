8e00fae4b49e31b76718d3cd7f5ef167c941cf62
杨华奇 <541871045@qq.com>
2017-06-29 18:07:43 +0800

    eureka客户端

---
+++ eureka-client/src/main/java/com/yhq/eureka/client/Application.java
@@ -0,0 +1,14 @@
+package com.yhq.eureka.client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+
+	}
+}
---
+++ eureka-client/src/main/java/com/yhq/eureka/client/controller/EurekaClientController.java
@@ -0,0 +1,20 @@
+package com.yhq.eureka.client.controller;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HuaQi.Yang
+ * @date 2017年6月29日
+ */
+@EnableEurekaClient
+@RestController
+@RequestMapping("/eureka/client/")
+public class EurekaClientController {
+	@RequestMapping("hello")
+	public String home() {
+		return "Hello world";
+	}
+
+}
