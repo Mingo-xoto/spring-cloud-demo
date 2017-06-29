6f90688c90cca6e32756b38f24dad9606c1748d5
杨华奇 <541871045@qq.com>
2017-06-29 16:55:06 +0800

    spring cloud eureka集群和独立模式搭建

---
+++ eureka-server-standalone/src/main/java/com/yhq/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.yhq.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author HuaQi.Yang
+ * @date 2017年6月29日
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
