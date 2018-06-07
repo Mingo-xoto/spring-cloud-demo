a6ef3f66b934bd9443992a3705455216d7e99924
YHQ <YHQ>
2018-06-07 15:50:45 +0800

    mq管理界面发布消息客户端接收问题：发布的默认为bytep[]字节数组，接收的时候用String等非byte[]接收会报错且一直重试

--- bus-common/src/main/java/com/yhq/config/RabbitMqConfig.java
+++ bus-common/src/main/java/com/yhq/config/RabbitMqConfig.java
@@ -1,6 +1,10 @@
 package com.yhq.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +13,20 @@ public class RabbitMqConfig {
     @Bean
     public Queue helloQueue() {
         return new Queue("hello");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 }
--- bus-common/src/main/java/com/yhq/config/Receiver.java
+++ bus-common/src/main/java/com/yhq/config/Receiver.java
@@ -1,14 +1,29 @@
 package com.yhq.config;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = "hello")
+@RabbitListener(queues = "queue1")
 public class Receiver {
-    @RabbitHandler
-    public void process(String msg) {
-        System.out.println("Receiver:"+msg);
+
+    /**
+     * 控制台非指定消息接收类全限定名
+     * @param msg
+     */
+    @RabbitListener(queues = "hello", containerFactory="rabbitListenerContainerFactory")
+    public void processWithBytes(byte[] msg) {
+        System.out.println("==消费消息->>>>>"+new String(msg));
     }
+
+    /**
+     * 控制台指定消息接收类全限定名
+     * @param msg
+     */
+    @RabbitListener(queues = "queue1", containerFactory="rabbitListenerContainerFactory")
+    public void processWithString(@Payload String msg) {
+        System.out.println("--消费消息->>>>>"+msg);
+    }
+
 }
