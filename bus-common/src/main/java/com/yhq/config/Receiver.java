5c5c6c0a10e61a876a8ff3659c1de9a36935f4f2
YHQ <YHQ>
2018-06-08 10:25:37 +0800

    mq发送消息

--- bus-common/src/main/java/com/yhq/config/Receiver.java
+++ bus-common/src/main/java/com/yhq/config/Receiver.java
@@ -1,5 +1,6 @@
 package com.yhq.config;
 
+import com.yhq.msg.MessageDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,35 @@ import org.springframework.stereotype.Component;
 @RabbitListener(queues = "queue1")
 public class Receiver {
 
+//    /**
+//     * 控制台非指定消息接收类全限定名
+//     * @param msg
+//     */
+//    @RabbitListener(queues = "hello", containerFactory="rabbitListenerContainerFactory")
+//    public void processWithBytes(byte[] msg) {
+//        System.out.println("==消费消息->>>>>"+new String(msg));
+//    }
+
     /**
      * 控制台非指定消息接收类全限定名
      * @param msg
      */
     @RabbitListener(queues = "hello", containerFactory="rabbitListenerContainerFactory")
-    public void processWithBytes(byte[] msg) {
-        System.out.println("==消费消息->>>>>"+new String(msg));
+    public void processMessage(MessageDto msg) {
+        System.out.println("==消费消息->>>>>"+msg.getName());
     }
+/*
+    */
+/**
+     * 控制台非指定消息接收类全限定名
+     * @param msg
+     *//*
+
+    @RabbitListener(queues = "hello", containerFactory="rabbitListenerContainerFactory")
+    public void processMessage(Message msg) {
+        System.out.println("==消费消息->>>>>"+new String(msg.getBody()));
+    }
+*/
 
     /**
      * 控制台指定消息接收类全限定名
--- bus-common/src/main/java/com/yhq/config/Sender.java
+++ bus-common/src/main/java/com/yhq/config/Sender.java
@@ -1,18 +1,23 @@
 package com.yhq.config;
 
+import com.yhq.msg.MessageDto;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
-@Component
+@RestController
+@RequestMapping("/mq/")
 public class Sender {
     @Autowired
     private AmqpTemplate rabbitTemplate;
+
+    @RequestMapping("send")
     public void send() {
-        String msg = "hello rabbitmq:"+new Date();
-        System.out.println("Sender:"+msg);
-        this.rabbitTemplate.convertAndSend("hello", msg);
+        MessageDto dto = new MessageDto();
+        dto.setName("fuck you man !");
+//        String msg = "hello rabbitmq:"+new Date();
+//        System.out.println("Sender:"+msg);
+        this.rabbitTemplate.convertAndSend("hello", dto);
     }
 }
